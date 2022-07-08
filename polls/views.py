5485971ea354ea14b7897dfc3bc7868b43864839
David Aydin <david.aydin6@gmail.com>
2022-07-08 10:42:38 -0400

    Django app part 6 complete, fixed detail page display

--- polls/views.py
+++ polls/views.py
@@ -45,8 +45,8 @@ class IndexView(generic.ListView):
         return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
-    # model = Question
-    # template_name = 'polls/detail.html'
+    model = Question
+    template_name = 'polls/detail.html'
     def get_queryset(self):
         # Excludes any questions that aren't publshed yet
         return Question.objects.filter(pub_date__lte=timezone.now())
