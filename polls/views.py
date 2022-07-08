19fddd749c2d90801a775d097caad2821e5296f8
David Aydin <david.aydin6@gmail.com>
2022-07-07 22:29:25 -0400

    Django app part 5 completed --Note at bottom of tests.py

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,9 @@
 # from django.http import Http404, HttpResponse
-from django.template import loader
-from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
+# from django.template import loader
+from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.utils import timezone
 from django.views import generic
 
 from .models import Question, Choice
@@ -41,11 +42,14 @@ class IndexView(generic.ListView):
     
     def get_queryset(self):
         # Returns last 5 published
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
-    model = Question
-    template_name = 'polls/detail.html'
+    # model = Question
+    # template_name = 'polls/detail.html'
+    def get_queryset(self):
+        # Excludes any questions that aren't publshed yet
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 class ResultsView(generic.DetailView):
     model = Question
